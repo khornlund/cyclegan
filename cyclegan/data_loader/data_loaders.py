efa46a3427ddb5d281caebde1473a9ad84c6356b
Karl Hornlund <karlhornlund@gmail.com>
2019-04-20 14:54:43 +0930

    Added automatic download of berkeley datasets

--- cyclegan/data_loader/data_loaders.py
+++ cyclegan/data_loader/data_loaders.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import random
+import subprocess
 
 from torchvision import transforms
 from torch.utils.data import Dataset
@@ -9,22 +10,44 @@ from PIL import Image
 from cyclegan.base import BaseDataLoader
 
 
-class Apples2OrangesDataLoader(BaseDataLoader):
+class BerkelyDataLoader(BaseDataLoader):
 
-    def __init__(self, data_dir, batch_size, img_size, shuffle, validation_split,
+    options = [
+        'apple2orange',
+        'summer2winter_yosemite',
+        'horse2zebra',
+        'monet2photo',
+        'cezanne2photo',
+        'ukiyoe2photo',
+        'vangogh2photo',
+        'maps',
+        'cityscapes',
+        'facades',
+        'iphone2dslr_flower',
+        'ae_photo'
+    ]
+
+    def __init__(self, data_dir, dataset, batch_size, img_size, shuffle, validation_split,
                  num_workers, training=True):
         transforms_ = [
             transforms.Resize(int(img_size * 1.12), Image.BICUBIC),
             transforms.RandomCrop(img_size),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ]
+        data_f = os.path.join(data_dir, dataset)
+        if not os.path.exists(data_f):
+            self.download_dataset(dataset)
+        self.dataset = ImageDataset(data_f, transforms_=transforms_, unaligned=True)
 
-        self.dataset = ImageDataset(data_dir, transforms_=transforms_, unaligned=True)
-
-        super(Apples2OrangesDataLoader, self).__init__(
+        super(BerkelyDataLoader, self).__init__(
             self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+    def download_dataset(self, dataset):
+        if dataset not in self.options:
+            raise Exception(f'{dataset} is not a valid dataset. Options are {self.options}')
+        subprocess.call(f'./download_dataset {dataset}', shell=True)
 
 
 class ImageDataset(Dataset):
